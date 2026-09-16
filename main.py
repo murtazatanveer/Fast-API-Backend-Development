4763958a207cb7bec573938cb402dffe1e0832aa
Murtaza_Tanveer <murtaza.tanveer488@gmail.com>
2026-09-16 18:48:44 +0500

    My First Fast API Project.

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI;
+
+app = FastAPI()
+@app.get("/")
+def hello():
+    return {"message":"Hello World"}
+
+@app.get("/about")
+def about():
+    return {"message":"My name is Murtaza Tanveer"}
