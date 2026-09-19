43d350d0712fa7cfaa6620ce1efdd74bb7367ee6
Murtaza_Tanveer <murtaza.tanveer488@gmail.com>
2026-09-19 09:09:20 +0500

    Field Validation in Pydantic

--- PydanticTutorials/1_pydantic_why.py
+++ PydanticTutorials/1_pydantic_why.py
@@ -1,5 +1,6 @@
-from pydantic import BaseModel, EmailStr, AnyUrl, Field
+from pydantic import BaseModel, EmailStr, AnyUrl, Field, field_validator
 from typing import List, Dict, Optional, Annotated
+
 class Patient(BaseModel):
     name: Annotated[str,Field(max_length=50,title="Patient Name",description="The full name of the patient")]
     age: Annotated[int,Field(gt=0, lt=81,strict=True)]
@@ -7,14 +8,24 @@ class Patient(BaseModel):
     linkedinUrl: Annotated[Optional[AnyUrl] , Field(default=None)]
     isMarried: Annotated[Optional[bool],Field(default=True,description="Is the Petient Married or not")];
     weight: Annotated[float , Field(gt=3)]
-
     allergies: Annotated[Optional[List[str]] , Field(default=None, max_length=5)]
-
     contactDetails: Dict[str, str]
 
+    @field_validator("email")
+    @classmethod
+    def email_validator(cls,value,):
 
-def addPatientData(patient: Patient):
-    print(patient, "\nData Added")
+        validDomainNames=["ici.com","hbl.com"]
+
+        if value.split("@")[-1] not in validDomainNames:
+            raise ValueError("Not a Valid Domain")
+
+        return value;
+
+    @field_validator("name",mode="before")
+    @classmethod
+    def transformName(cls,value):
+        return value.upper()
 
 
 patientData = {
@@ -22,7 +33,7 @@ patientData = {
     "age": 23,
     "linkedinUrl": "https://www.linkedin.com/",
     "isMarried":False,
-    "email": "murtaza488@gmail.com",
+    "email": "murtaza@ici.com",
     "weight": 70,
     "allergies": ["Avocado", "Venoms", "mold spores"],
     "contactDetails": {
@@ -32,4 +43,7 @@ patientData = {
 }
 
 p1 = Patient(**patientData)
+
+def addPatientData(patient: Patient):
+    print(patient, "\nData Added")
 addPatientData(p1)
