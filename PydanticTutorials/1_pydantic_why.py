982d7f1626c3dc60e9eff6913e8dcc2f4fa68083
Murtaza_Tanveer <murtaza.tanveer488@gmail.com>
2026-09-19 08:37:58 +0500

    Type Validation and Data Validation in Pydantic.

---
+++ PydanticTutorials/1_pydantic_why.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel, EmailStr, AnyUrl, Field
+from typing import List, Dict, Optional, Annotated
+class Patient(BaseModel):
+    name: Annotated[str,Field(max_length=50,title="Patient Name",description="The full name of the patient")]
+    age: Annotated[int,Field(gt=0, lt=81,strict=True)]
+    email: EmailStr
+    linkedinUrl: Annotated[Optional[AnyUrl] , Field(default=None)]
+    isMarried: Annotated[Optional[bool],Field(default=True,description="Is the Petient Married or not")];
+    weight: Annotated[float , Field(gt=3)]
+
+    allergies: Annotated[Optional[List[str]] , Field(default=None, max_length=5)]
+
+    contactDetails: Dict[str, str]
+
+
+def addPatientData(patient: Patient):
+    print(patient, "\nData Added")
+
+
+patientData = {
+    "name": "Murtaza",
+    "age": 23,
+    "linkedinUrl": "https://www.linkedin.com/",
+    "isMarried":False,
+    "email": "murtaza488@gmail.com",
+    "weight": 70,
+    "allergies": ["Avocado", "Venoms", "mold spores"],
+    "contactDetails": {
+        "cell no": "12345678",
+        "address": "Abbottabad"
+    }
+}
+
+p1 = Patient(**patientData)
+addPatientData(p1)
